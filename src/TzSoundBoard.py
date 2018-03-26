b748a52f71aee357023bc483796484bb92cb2931
MC <taggedzi.mpc@gmail.com>
2018-03-26 01:10:04 -0400

    [WIP] Refactored - Crated WindowController Class to control gui elements - Created Sound Class to control sound elements - Updated Requirements to include pygame
    - Added frames to make ui nicer
    - Decided to make number of buttons per row fixed for now (default = 4) will change later if needed.
    - Decided to make volume scale inverted vertical
    - Still tons to do :)

--- src/TzSoundBoard.py
+++ src/TzSoundBoard.py
@@ -1,6 +1,6 @@
 import os
 import pygame
-import mutagen # added for playback length etc. possible slider to indicate play position.
+# import mutagen # added for playback length etc. possible slider to indicate play position.
 
 try:
     # python 3
@@ -19,13 +19,33 @@ class WindowController(object):
     def __init__(self):
         self.menu = None
         self.volume = None
-        self.volume_callback = lambda x: print(x)
-        self.pick_file_callback = lambda x: print(x)
+        self.sounds = []
         self.main_window = tkinter.Tk()
         self.main_window.geometry("500x300+150+150")
         self.main_window.title('TzSoundBoard')
-        self.volume_controls(3, 0)
         self.create_menu()
+        self.tools = self.set_tool_frame()
+        self.sound_frame = self.set_sound_frame()
+        self.set_volume(100)
+
+    def set_tool_frame(self):
+        tools = tkinter.LabelFrame(self.main_window, text="Tools", borderwidth=1, relief="groove", width=200, padx=5,
+                                   pady=5)
+        tools.grid(row=0, column=0, sticky="ne")
+
+        # add volume controlls
+        self.volume = tkinter.Scale(tools, label="Volume", orient=tkinter.VERTICAL, from_=100, to=0, command=self.set_volume)
+        self.volume.grid(row=1, column=0)
+
+        # add: add_sound controls.
+        button = tkinter.Button(tools, text="Add New Sound", height=2, command=self.pick_file)
+        button.grid(row=0, column=0)
+        return tools
+
+    def set_sound_frame(self):
+        sound_frame = tkinter.Frame(self.main_window, padx=5, pady=5)
+        sound_frame.grid(row=0, column=1, sticky="nsew")
+        return sound_frame
 
     def main(self):
         self.main_window.mainloop()
@@ -33,16 +53,22 @@ class WindowController(object):
     def close(self):
         self.main_window.destroy()
 
-    def add_button(self, label, row, col, callback):
-        button = tkinter.Button(self.main_window, text=label, height=10, width=20, command=callback)
-        button.grid(row=row, column=col)
-
     def pick_file(self):
         file_path = filedialog.askopenfilename(initialdir='./', title="Select file", filetypes=self.MUSIC_FILE_TYPES)
-        self.pick_file_callback(file_path)
-
-    def set_pick_file_callback(self, callback):
-        self.pick_file_callback = callback
+        if file_path:
+            new_sound = Sound(file_path, os.path.basename(file_path), "")
+            self.sounds.append(new_sound)
+            total_sounds = len(self.sounds) - 1
+            rows = int(total_sounds / 4)
+            if total_sounds % 4 == 0:
+                col = 0
+            else:
+                col = int(total_sounds % 4)
+            button = tkinter.Button(self.sound_frame, text=os.path.basename(file_path), height=10, width=20)
+            button.grid(row=rows, column=col)
+            button.config(command=new_sound.play)
+        else:
+            raise NotImplementedError("No file Selected")
 
     def create_menu(self):
         self.menu = tkinter.Menu(self.main_window)
@@ -54,29 +80,8 @@ class WindowController(object):
         self.menu.add_cascade(label="File", menu=file)
 
     def set_volume(self, value):
-        value = int(value)
-        print("is called: {}".format(value))
-        if value == 0:
-            self.volume = 0
-        else:
-            self.volume = value / 100
-        self.volume_callback(self.volume)
-
-    def set_volume_callback(self, callback):
-        self.volume_callback = callback
-
-    def volume_controls(self, row, col):
-        scale = tkinter.Scale(self.main_window, label="Volume", orient=tkinter.HORIZONTAL, from_=0, to=100, command=self.set_volume)
-        scale.grid(row=row, column=col)
-
-
-class TzSoundBoard(object):
-
-    def __init__(self):
-        self.column = 0
-        self.row = 0
-
-# my = TzSoundBoard()
+        self.volume.set(value)
+        Sound.set_volume(value)
 
 
 class Sound(object):
@@ -85,29 +90,26 @@ class Sound(object):
         self.__sound_path = None
         self.__label = None
         self.__image_path = None
+        self.is_playing = False
         self.sound_path = sound_path
         self.label = label
         self.image_path = image_path
-        pygame.mixer.init()
 
-    def play(self, file_to_play):
-        pygame.mixer.music.load(file_to_play)
+    def play(self):
+        pygame.mixer.music.load(self.sound_path)
         pygame.mixer.music.play()
 
     def stop(self):
         pygame.mixer.music.stop()
 
-    def set_volume(self, value):
+    @classmethod
+    def set_volume(cls, value):
         value = int(value)
         print("is called: {}".format(value))
         if value == 0:
             pygame.mixer.music.set_volume(0)
         else:
             pygame.mixer.music.set_volume(value/100)
-
-    def generate_button(self, window, file_to_play):
-        return tkinter.Button(window, text=self.label, height=10, width=20,
-                              command=lambda: self.play(file_to_play)).grid(row=self.row, column=self.column)
 
     @property
     def sound_path(self):
@@ -146,11 +148,6 @@ class Sound(object):
         del self.__image_path
 
 
-def double(val):
-    print(str(val), str(val))
-
-
+pygame.mixer.init()
 wc = WindowController()
-wc.set_pick_file_callback(double)
-wc.set_volume_callback(double)
 wc.main()
